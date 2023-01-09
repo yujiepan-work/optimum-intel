74016a4be1628e5d042c196e20f6b88cf6aaee1b
Vui Seng Chua <vui.seng.chua@intel.com>
2023-01-09 10:00:39 -0800

    Move openvino-dev dependency to be extra of NNCF

--- setup.py
+++ setup.py
@@ -30,9 +30,10 @@ QUALITY_REQUIRES = [
 
 EXTRAS_REQUIRE = {
     "neural-compressor": ["neural-compressor>=2.0.0", "onnx", "onnxruntime"],
-    "openvino": ["openvino-dev>=2022.3.0", "transformers>=4.20.0", "onnx"],
+    "openvino": ["openvino>=2022.3.0", "transformers>=4.20.0", "onnx"],
     "nncf": [
-        "nncf @ git+https://github.com/openvinotoolkit/nncf"
+        "nncf @ git+https://github.com/openvinotoolkit/nncf",
+        "openvino-dev>=2022.3.0",
     ],  # TODO: make nncf>=2.4 once it is released. JPQD requires this version or current develop branch
     "ipex": ["intel_extension_for_pytorch"],
     "diffusers": ["diffusers"],
