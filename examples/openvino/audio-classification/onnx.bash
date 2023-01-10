95073920010674720df36d25c20cee16bdcf7725
Pan, Yujie <yujie.pan@intel.com>
2023-01-10 06:16:37 -0800

    onnx inference

---
+++ examples/openvino/audio-classification/onnx.bash
@@ -0,0 +1,27 @@
+python run_audio_classification_val.py \
+    --model_name_or_path facebook/wav2vec2-base \
+    --nncf_compression_config /nvme2/yujiepan/workspace/jpqd-test/LOGS/optimum-ac/audio/0110qat_QuantInput_lr3e-05_epoch12/wav2vec2-base-qat-quant-input.json \
+    --dataset_name superb \
+    --dataset_config_name ks \
+    --output_dir /tmp/yujiepan/temp_wav2vec2-base-ft-keyword-spotting \
+    --overwrite_output_dir \
+    --remove_unused_columns False \
+    --do_eval \
+    --max_steps 1 \
+    --learning_rate 0.0 \
+    --max_length_seconds 1 \
+    --attention_mask False \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 4 \
+    --per_device_eval_batch_size 64 \
+    --dataloader_num_workers 4 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model accuracy \
+    --save_total_limit 3 \
+    --seed 0 \
