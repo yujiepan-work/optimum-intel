68fdabe5033d8ae8736fd78b4cd51131a4dd0eb2
Pan, Yujie <yujie.pan@intel.com>
2023-03-16 14:04:38 +0800

    bugfix

--- tests/openvino/test_training.py
+++ tests/openvino/test_training.py
@@ -478,7 +478,7 @@ class OVTrainerTextClassificationTrainingTest(OVTrainerBaseTrainingTest):
     def check_ovmodel_output_equals_torch_output(self, ovmodel, torch_model):
         torch_model = torch_model.eval()
         for max_seq_length in [16, 89, 128]:
-            for batch_size in [1, 3, 7, 11]:
+            for batch_size in [1, 3, 7]:
                 examples = self.dataset["train"].sort("sentence")[:batch_size]
                 inputs = self.tokenizer(
                     examples["sentence"],
@@ -640,7 +640,7 @@ class OVTrainerImageClassificationTrainingTest(OVTrainerBaseTrainingTest):
                     torch.allclose(
                         torch.softmax(ovmodel_logits, dim=-1),
                         torch.softmax(torch_logits, dim=-1),
-                        atol=0.001,  # TODO: swin has higher errors
+                        atol=0.0001,  # TODO: swin has higher errors
                     )
                 )
 
